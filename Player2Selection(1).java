acc4b64ddbf3d44f169563cae0a66284ac6ee0ba
Touya-Rei <106257286+Touya-Rei@users.noreply.github.com>
2022-06-07 10:47:23 -0400

    restoring files

---
+++ Player2Selection(1).java
@@ -0,0 +1,20 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Player2Selection here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Player2Selection extends World
+{
+
+    /**
+     * Constructor for objects of class Player2Selection.
+     * 
+     */
+    public Player2Selection()
+    {    
+        super(1000, 600, 1); 
+    }
+}
