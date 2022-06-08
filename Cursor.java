dcfe8da97ad9c46d46067fb1cf292cbc4c3c48da
Touya-Rei <106257286+Touya-Rei@users.noreply.github.com>
2022-06-08 09:39:26 -0400

    Finished selection worlds

--- Cursor.java
+++ Cursor.java
@@ -11,13 +11,25 @@ public class Cursor extends Actor
     int dx=0;
     int dy=0;
     boolean selected = false;
+    boolean selected2 = false;
     TankClasses tank;
+    int select=0;
+    int select2=0;
     public void act()
     {
         
         dx=0;
         dy=0;
-        movement();
+         if (getWorld() instanceof Player1Selection)
+        {
+            movement();          
+        } 
+         if (getWorld() instanceof Player2Selection)
+        {
+            movement2();          
+        } 
+        classes();
+        classes2();
         setLocation(getX()+dx, getY()+dy);
         
         
@@ -28,6 +40,11 @@ public class Cursor extends Actor
          if (selected==true&&getWorld() instanceof Player1Selection)
         {
             Greenfoot.setWorld(new Player2Selection());          
+        } 
+        
+         if (selected2==true&&getWorld() instanceof Player2Selection)
+        {
+            Greenfoot.setWorld(new MyWorld());          
         } 
          
     }
@@ -58,14 +75,97 @@ public class Cursor extends Actor
         {
             selected=true;
         }
+        
+        
     }
     
     public void classes()
     {
-        Actor Amogus = getOneIntersectingObject(AmogusMKII.class);
-        if (Amogus!= null)
+        if (Greenfoot.isKeyDown("enter")&&isTouching(AmogusMKII.class))
         {
-            
+            select=1;
+        }
+        
+        if (Greenfoot.isKeyDown("enter")&&isTouching(BigChonkers.class))
+        {
+            select=2;
+        }
+        
+        if (Greenfoot.isKeyDown("enter")&&isTouching(Bob.class))
+        {
+            select=3;
+        }
+        
+        if (Greenfoot.isKeyDown("enter")&&isTouching(GlassCannon.class))
+        {
+            select=4;
+        }
+        
+        if (Greenfoot.isKeyDown("enter"))
+        {
+            System.out.println(""+select);
         }
     }
+    
+    //--------------------------------------------------------------------------
+    public void movement2()
+    {
+        if (Greenfoot.isKeyDown("left"))
+        {
+            dx=-5;
+        }
+        
+        if (Greenfoot.isKeyDown("right"))
+        {
+            dx=5;
+        }
+        
+        if (Greenfoot.isKeyDown("up"))
+        {
+            dy=-5;
+        }
+        
+        if (Greenfoot.isKeyDown("down"))
+        {
+            dy=5;
+        }
+        
+        if (Greenfoot.isKeyDown("enter")&&isTouching(TankClasses.class)
+        &&getWorld() instanceof Player2Selection)
+        {
+            selected2=true;
+        }
+        
+        
+    }
+    
+    public void classes2()
+    {
+        if (Greenfoot.isKeyDown("enter")&&isTouching(AmogusMKII.class))
+        {
+            select2=1;
+        }
+        
+        if (Greenfoot.isKeyDown("enter")&&isTouching(BigChonkers.class))
+        {
+            select2=2;
+        }
+        
+        if (Greenfoot.isKeyDown("enter")&&isTouching(Bob.class))
+        {
+            select2=3;
+        }
+        
+        if (Greenfoot.isKeyDown("enter")&&isTouching(GlassCannon.class))
+        {
+            select2=4;
+        }
+        
+        if (Greenfoot.isKeyDown("enter"))
+        {
+            System.out.println(""+select2);
+        }
+    }
+    
+    
 }
