8b0276e7814903ccbb5b0130a6b7ab930c407e39
Touya-Rei <106257286+Touya-Rei@users.noreply.github.com>
2022-06-09 08:37:40 -0400

    Set up values for Player Classes

--- Bob.java
+++ Bob.java
@@ -8,10 +8,10 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Bob extends TankClasses
 {
-    /**
-     * Act - do whatever the Bob wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    int HP=10;
+    int speedX = 5;
+    int speedY = 0;
+    int damage=1;
     public void act()
     {
         // Add your action code here.
