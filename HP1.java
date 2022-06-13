7db68529b33ca219fb5a0d000eb3295af75a4a41
Touya-Rei <106257286+Touya-Rei@users.noreply.github.com>
2022-06-13 09:53:01 -0400

    Collision Improvement and live HP updates Improvements

--- HP1.java
+++ HP1.java
@@ -25,7 +25,7 @@ public class HP1 extends HPBars
         }
         else if (player.HP==0)
         {
-            setImage("folder/hp0.png");
+            setImage("folder/0.png");
         }
     }
 }
--- MyWorld.java
+++ MyWorld.java
@@ -28,4 +28,7 @@ public class MyWorld extends World
     }
     
     
+    
+    
+    
 }
